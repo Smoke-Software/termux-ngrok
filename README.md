9f532923a8fe8bc58c11ef5401ae10eb166a7f3e
Smoke-Software <98212561+Smoke-Software@users.noreply.github.com>
2022-01-28 14:12:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# termux-ngrok
+Ngrok - очень полезная утилита, при помощи которой можно сервера, работающие на локальной сети переносить в интернет, чтобы мог любой на них зайти
+важен, при создании фишинг страниц
+
+Скрипт перевел: https://t.me/lamer112311 специально для: https://t.me/smoke_software
+
+###Установка на Termux
+```
+apt update && apt upgrade
+apt install git
+git clone https://github.com/Smoke-Software/termux-ngrok.git
+cd termux-ngrok
+chmod +x termux-ngrok.sh
+./termux-ngrok.sh
+```
+### Скриншоты
+
+<p align="centre">
+<img src="https://i.imgur.com/86lzmIf.png" alt="Script">
+</p>
+
+<p align="centre">
+<img src="https://i.imgur.com/sRb2y4P.png" alt="Script">
+</p>
+
+<p align="centre">
+<img src="https://i.imgur.com/m80I9UB.png" alt="Script">
+</p>
+
+
+
---
+++ termux-ngrok.sh
@@ -0,0 +1,68 @@
+#!/bin/dash
+# Утилиту перевел: https://t.me/lamer112311 для https://t.me/smoke_software
+
+# Оригинальный репозиторий: https://github.com/Marcel0Sousa/termux-ngrok
+# Original repo: https://github.com/Marcel0Sousa/termux-ngrok
+
+atualizar='\033[1;32m'
+echo "$atualizar Обновление репозитория"
+apt update && upgrade -y
+apt install -y wget
+clear
+k='\033[01;32m'
+echo
+echo "${k}████████╗ ██████╗██╗  ██╗███████╗██╗      ██████╗ ";
+echo "${k}╚══██╔══╝██╔════╝██║  ██║██╔════╝██║     ██╔═══██╗";
+echo "${k}   ██║   ██║     ███████║█████╗  ██║     ██║   ██║";
+echo "${k}   ██║   ██║     ██╔══██║██╔══╝  ██║     ██║   ██║";
+echo "${k}   ██║   ╚██████╗██║  ██║███████╗███████╗╚██████╔╝";
+echo "${k}   ╚═╝    ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ";
+echo "${k} /_/_/_/_[By @lamer112311 for @smoke_software]_/_/";
+echo
+echo "Установить Ngrok? [Y - да /n - нет]>> "
+read opcao
+case $opcao in
+y)
+echo
+echo "Установка Termux-ngrok..."
+case `dpkg --print-architecture` in
+aarch64)
+    architectureURL="arm64" ;;
+arm)
+    architectureURL="arm" ;;
+armhf)
+    architectureURL="armhf" ;;
+amd64)
+    architectureURL="amd64" ;;
+i*86)
+    architectureURL="i386" ;;
+x86_64)
+    architectureURL="amd64" ;;
+*)
+    echo "(X) Неизвестная архитектура системы (X)"
+esac
+
+wget "https://github.com/tchelospy/NgrokTest/blob/master/ngrok-stable-linux-${architectureURL}.zip?raw=true" -O ngrok.zip
+unzip ngrok.zip
+cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+chmod 700 /data/data/com.termux/files/usr/bin/ngrok
+rm ngrok ngrok.zip
+clear
+echo "${k}███╗   ██╗ ██████╗ ██████╗  ██████╗ ██╗  ██╗";
+echo "${k}████╗  ██║██╔════╝ ██╔══██╗██╔═══██╗██║ ██╔╝";
+echo "${k}██╔██╗ ██║██║  ███╗██████╔╝██║   ██║█████╔╝ ";
+echo "${k}██║╚██╗██║██║   ██║██╔══██╗██║   ██║██╔═██╗ ";
+echo "${k}██║ ╚████║╚██████╔╝██║  ██║╚██████╔╝██║  ██╗";
+echo "${k}╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝";
+echo
+echo "Пример команды (ngrok http 8080 \nИли ngrok для справки)"
+;;
+
+n)
+clear
+echo "Не удалось установить Ngrok"
+echo
+esac
+
+
+
